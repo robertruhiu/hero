1c19fc88e7cd03ab143d4cd544a2d5a06e4d9fc3
robertruhiu <robertruhiu@gmail.com>
2018-02-20 12:44:01 +0000

    base files

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1441711509288314',
+		'clientSecret': '72331b79ebc0d8402d98dd2d1960c9c9',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}

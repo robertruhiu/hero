cbc68dee59bff25ca17dcbe39ff393d4e0e82fdb
robertruhiu <robertruhiu@gmail.com>
2018-02-27 12:25:52 +0000

    portswitch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 8989;
 
 var cookieParser = require('cookie-parser');
 var session = require('express-session');

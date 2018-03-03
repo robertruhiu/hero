9f2eafa0f0a4453ac96f70e9c41d1d769db980e0
robertruhiu <robertruhiu@gmail.com>
2018-03-03 18:00:37 +0000

    cart configurations shift from ejs template to handlebars
    Signed-off-by: robertruhiu <robertruhiu@gmail.com>

--- app/routes.js
+++ app/routes.js
@@ -3,26 +3,29 @@ var Product = require('../app/models/products');
 var Cart = require('../app/models/cart');
 
 module.exports = function(app, passport){
+
 	app.get('/', function(req, res){
-		Product.find({},function (err,prds) {
-			res.render('index.ejs',{product:prds});
-
+		Product.find({},function (err,docs) {
+			var productChunks = [];
+			var chunkSize =3;
+			for (var i=0;i<docs.length;i+=chunkSize ){
+				productChunks.push(docs.slice(i,i+chunkSize));
+			}
+			res.render('index.hbs',{products:productChunks});
         });
-
-
 	});
 
 	app.get('/login', function(req, res){
-		res.render('login.ejs', { message: req.flash('loginMessage') });
+		res.render('login.hbs', { message: req.flash('loginMessage') });
 	});
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect: '/home',
+		successRedirect: '/',
 		failureRedirect: '/login',
 		failureFlash: true
 	}));
 
 	app.get('/signup', function(req, res){
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
+		res.render('signup.hbs');
 	});
 
 
@@ -33,43 +36,30 @@ module.exports = function(app, passport){
 	}));
 
 	app.get('/home', isLoggedIn, function(req, res){
-		res.render('home.ejs', { user: req.user });
+		res.render('home.hbs', { user: req.user });
 	});
 
 	app.get('/add-to-cart/:id',function (req,res) {
 		var productId = req.params.id;
 		var cart = new Cart(req.session.cart ? req.session.cart : {});
-
-
 		Product.findById(productId,function (err,product) {
 			if (err){
 				return res.redirect('/');
 			}
 			cart.add(product,product.id);
 			req.session.cart=cart;
-			console.log(req.session.cart);
-			quantity=req.session.cart.totalQty;
-			console.log(quantity);
-			return res.redirect('/')
+			console.log(cart);
+			res.redirect('/');
         });
-
-
     });
 
 	app.get('/profile', isLoggedIn, function(req, res){
-		Product.find({},function (err,prds) {
-			res.render('profile.ejs',{product:prds});
-
-        });
-
+		if (!req.session.cart) {
+            res.render('profile.hbs',{products:null});
+        }
+        var cart = new Cart(req.session.cart);
+		res.render('profile.hbs',{products:cart.generateArray(),totalPrice:cart.totalPrice});
 	});
-
-	app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
-
-	app.get('/auth/facebook/callback', passport.authenticate('facebook',
-		{ successRedirect: '/home',failureRedirect: '/' }));
-
-
 	app.get('/logout', function(req, res){
 		req.logout();
 		res.redirect('/');

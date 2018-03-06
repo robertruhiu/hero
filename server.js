173865cbb9f44751daf3768ecb2ab4a90f33ca2c
robertruhiu <robertruhiu@gmail.com>
2018-03-06 16:05:03 +0000

    start script
    Signed-off-by: robertruhiu <robertruhiu@gmail.com>

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use(function (req,res,next) {
 
 require('./app/routes.js')(app, passport);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
